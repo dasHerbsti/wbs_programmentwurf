c1c68a262606dd8068f713d1616bd36f3cbad63c
dasHerbsti <carinaherbst1994@googlemail.com>
2018-12-06 18:17:39 +0100

    add person id attribute to attributevalue class

--- evidence/src/main/java/evidence/AttributeValue.java
+++ evidence/src/main/java/evidence/AttributeValue.java
@@ -3,12 +3,14 @@ package evidence;
 public class AttributeValue { 
     private final String _value; 
     private final String _attributeName; 
-    private final Integer _bookCode; 
+    private final int _bookCode;
+    private final String _personID;
 
-    public AttributeValue(String value, String attributeName, Integer bookCode) { 
+    public AttributeValue(String value, String attributeName, int bookCode, String personID) { 
       this._value=value;
       this._attributeName=attributeName;
       this._bookCode=bookCode;  
+      this._personID=personID;
     } 
 
     public String getValue(){
@@ -19,7 +21,11 @@ public class AttributeValue {
       return _attributeName;
     }
 
-    public Integer getBookCode(){
+    public int getBookCode(){
       return _bookCode;
+    }
+
+    public String getPersonNumber(){
+      return _personID;
     }
   } 
